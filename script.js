a7f31f3234b5afaab7413762f5d348d5da8fe44c
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2020-12-18 11:01:38 -0800

    fixing script file.

---
+++ script.js
@@ -0,0 +1,11 @@
+var author = ""
+var queryURL = "https://api.nytimes.com/svc/books/v3/reviews.json?author=" + author + "&api-key=qXumAozqy9Go05UQAhGponS62qp0WILb"
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
